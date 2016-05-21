df163d5e892e8c97853e3f7228bd249832a8a402
hades <i@mayuko.cn>
2016-05-21 17:23:12 +0800

    archive templet

--- archive.php
+++ archive.php
@@ -1,49 +1,40 @@
 <?php
+/*
+Template Name: archives
+*/
+?>
+
+<?php
 /**
- * The template for displaying Archive pages.
+ * The template for displaying all pages.
  *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
  *
  * @package Scrawl
  */
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+                       
+			<?php while ( have_posts() ) : the_post(); ?>
+                 
+                                <?php zww_archives_list(); ?>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 				?>
-			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
+			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
