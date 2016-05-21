6ae3036dbb14bd14134a881a19a3d788636718c0
hades <i@mayuko.cn>
2016-05-21 17:16:56 +0800

    changed the header&footer

--- header.php
+++ header.php
@@ -14,37 +14,43 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<SCRIPT language=javascript>
-function show_date_time(){
-window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("7/1/2015 18:05:30");//月，日，年，小时，分钟，秒
-today=new Date();
-timeold=(today.getTime()-BirthDay.getTime());
-sectimeold=timeold/1000
-secondsold=Math.floor(sectimeold);
-msPerDay=24*60*60*1000
-e_daysold=timeold/msPerDay
-daysold=Math.floor(e_daysold);
-e_hrsold=(e_daysold-daysold)*24;
-hrsold=setzero(Math.floor(e_hrsold));
-e_minsold=(e_hrsold-hrsold)*60;
-minsold=setzero(Math.floor((e_hrsold-hrsold)*60));
-seconds=setzero(Math.floor((e_minsold-minsold)*60));
-span_dt_dt.innerHTML="已运行"+daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒";
-}
-function setzero(i){
-if (i<10)
-{i="0" + i};
-return i;
-}
-show_date_time();
-</SCRIPT>
+
 <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/prism.css" type="text/css" media="screen" />
 <script type="text/javascript" src=<?php bloginfo('template_url'); ?>/prism.js"></script>
 <link href="/wp-content/themes/scrawl/prism.css" rel="stylesheet" />
 <script type="text/javascript" src="https://blog.mayuko.cn/siyue-api/?encode=js&charset=utf-8"></script>
 <?php wp_head(); ?>
-<?php wp_head(); ?>
+
+
+
+<script type="text/javascript">
+jQuery(document).ready(function($){
+ //===================================存档页面 jQ伸缩
+     (function(){
+         $('#al_expand_collapse,#archives span.al_mon').css({cursor:"s-resize"});
+         $('#archives span.al_mon').each(function(){
+             var num=$(this).next().children('li').size();
+             var text=$(this).text();
+             $(this).html(text+'<em> ( '+num+' 篇文章 )</em>');
+         });
+         var $al_post_list=$('#archives ul.al_post_list'),
+             $al_post_list_f=$('#archives ul.al_post_list:first');
+         $al_post_list.hide(1,function(){
+             $al_post_list_f.show();
+         });
+         $('#archives span.al_mon').click(function(){
+             $(this).next().slideToggle(400);
+             return false;
+         });
+         $('#al_expand_collapse').toggle(function(){
+             $al_post_list.show();
+         },function(){
+             $al_post_list.hide();
+         });
+     })();
+ });
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -65,7 +71,7 @@ show_date_time();
 			<?php wp_nav_menu( array( 'theme_location' => 'social', 'depth' => 1, 'link_before' => '<span class="screen-reader-text">', 'link_after' => '</span>', 'container_class' => 'social-links', ) ); ?>
 		<?php endif; ?>
 
-		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		
 
 		<?php if ( has_nav_menu( 'primary' ) ) : ?>
 			<nav id="site-navigation" class="main-navigation" role="navigation">
@@ -90,7 +96,7 @@ show_date_time();
 					scrawl_get_gravatar();
 				} 
 			?>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title" style="text-align:center"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 		</div>
 
 	</header><!-- #masthead -->
